7c0ea07555963b1f8570b50062c3767dfebcd9e6
jyothimukala555 <138197142+jyothimukala555@users.noreply.github.com>
2023-08-02 23:55:18 +0530

    Create OddNumberUsingStreams.java

---
+++ OddNumberUsingStreams.java
@@ -0,0 +1,28 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OddNumberUsingStreams {
+	
+	public static void main (String [] args) {
+	
+		List <Integer> list = new ArrayList<Integer>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		list.add(7);
+		list.add(8);
+		list.add(9);
+		
+		list = list.stream().filter(n -> n%2!=0).collect(Collectors.toList());
+		list.forEach(System.out::println);
+		
+	}
+
+}
